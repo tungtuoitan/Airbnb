bbb758dd710c9f3d3006c5e3822a7b07c3c254d1
Tung Le <hoanhtungle@gmail.com>
2023-09-03 15:47:38 +0700

    tôi chịu thua cursor-when-draggin

--- src/components/leftSlider.js
+++ src/components/leftSlider.js
@@ -9,25 +9,36 @@ export default function LeftSlider() {
   const leftSliderValue = useSelector(
     (state) => state.filterSlice.leftSliderValue
   );
+  const handleOnDragStart=(e)=>{
+    // e.preventDefault()
+    console.log('hi')
+      var emptyImage = new Image();
+      emptyImage.src = 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=';
+      e.dataTransfer.setDragImage(emptyImage, 0, 0);
+      e.dataTransfer.effectAllowed = 'all'
+      console.log(e.dataTransfer)
+      
+    };
+  
   
   const springs = useSpring({
     from: { x: 0 },
     to: { x: leftSliderValue },
+    immediate:true
   });
 
   return (
     <animated.div
       className={`w-c32 h-c32 || rounded-full bg-white ||
-        || border-c1 border-solid border-gray-300  cursor-pointer set-cursor
-        || box-shadow-u11
-        || relative top-c-16
+        || border-c1 border-solid border-gray-300  cursor-pointer set-cursor cursor-pointer
+        || box-shadow-u11 
+        || relative top-c-16  zmax3
         `}
-      style={{ ...springs }}
-      onTouchMove={(e) => handleOnTouchMove(e)}
+        draggable
+      style={{ ...springs,  }}
+      // onTouchMove={(e) => handleOnTouchMove(e)}
       onDrag={(e)=> handleOnDrag(e)}
-   
-
-
+      onDragStart={(e)=>handleOnDragStart(e)}
     />
    
   );
--- src/hooks/useCreateHandleDragLeft.js
+++ src/hooks/useCreateHandleDragLeft.js
@@ -5,8 +5,8 @@ export default function useCreateHandleDragLeft() {
   const dispatch = useDispatch();
   const displacement = useSelector((state) => state.filterSlice.displacement);
   return (e) => {
+    e.preventDefault()
     if (e.clientX !== 0) {
-      console.log('onDrag')
       const currentLeft = e.clientX - displacement - 16; // 16 = widthTarget(32) / 2
       dispatch(setLeftSlider(currentLeft));
     }
