49c4fce29d40be82ee24bdf97ed428e37449258c
Tung Le <hoanhtungle@gmail.com>
2024-05-28 13:59:57 +0700

    fix path again

--- src/components/ownerAvtContainer.js
+++ src/components/ownerAvtContainer.js
@@ -12,9 +12,10 @@ export default function OwnerAvtContainer() {
       break;
     }
   }
+  const lastChar = window.location.href[window.location.href.length -1 ]
   return (
     <div className="w-10 h-10   shadow-md first-letter:border-1 border-solid border-gray-400 rounded-full overflow-hidden ">
-      <img src={currentHomeList[index].owner.avatar} />
+      <img src={lastChar === "/" ? currentHomeList[index].owner.avatar.replace("Airbnb/",'') : currentHomeList[index].owner.avatar} />
     </div>
   );
 }
